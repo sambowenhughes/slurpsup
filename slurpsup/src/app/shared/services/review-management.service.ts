dbcd32b08901d703911918a293055f3180532c10
samdatashed <samdatashed@samdatasheds-MacBook-Pro.local>
2019-08-02 16:04:24 +0100

    Added styles to fron, linked service up with juice array

--- slurpsup/src/app/shared/services/review-management.service.ts
+++ slurpsup/src/app/shared/services/review-management.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnInit } from '@angular/core';
 import { Juice } from '../models/Juice';
+import { Subject } from '../../../../node_modules/rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,32 +8,34 @@ import { Juice } from '../models/Juice';
 export class ReviewManagementService implements OnInit {
 
   public juices: Array<Juice> = new Array;
+  public reviews : Juice[];
 
   constructor() { }
 
   ngOnInit(){
     console.log("Services being called")
-    this.getAllJuices();
+    this.getAllReviews();
   }
 
-  private getAllJuices(){
+  getAllReviews(){
     var juice1 = new Juice;
     juice1.name = "Innocent";
     juice1.lookAndFeel = 79;
     juice1.notes = "Really nice drink, flavour liked a long time";
     juice1.price = 30;
     juice1.reviewer = "Sam Bowen-Hughes";
-    juice1.taste = 86
+    juice1.taste = 16
 
     var juice2 = new Juice;
     juice2.name = "Coca Cola";
-    juice2.lookAndFeel = 58;
+    juice2.lookAndFeel = 38;
     juice2.notes = "Very sweet, lovely look. Not very good for you though";
-    juice2.price = 790;
+    juice2.price = 18;
     juice2.reviewer = "Sam Bowen-Hughes";
-    juice2.taste = 89
+    juice2.taste = 109
 
-    this.juices.push(juice1)
+    this.juices.push(juice1);
     this.juices.push(juice2);
   }
+
 }
